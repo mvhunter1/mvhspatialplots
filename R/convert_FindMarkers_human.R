577fdc24d7008f789c93ac6bc987736a10354756
Miranda Hunter <hunterm@mskcc.org>
2021-04-13 10:18:20 -0400

    commit

--- R/convert_FindMarkers_human.R
+++ R/convert_FindMarkers_human.R
@@ -1,89 +1,64 @@
 #' @title convert_FindMarkers_human
-#' @description convert the output of the Seurat function FindMarkers from fish genes to human.
-#' @param fish_genelist FindMarkers results dataframe.
-#' @param create_ranked_vector if T, will convert the output into a ranked vector with human genes as name and avg_logFC as values, to use for GSEA.
-#' @param remove_ribosomal_genes if T, will remove anything starting with rps or rpl from the gene list
+#' @description convert the results of FindMarkers (dataframe) to human genes and create ranked vector for GSEA. * Improved version.
+#' @param genelist FindMarkers output dataframe. Needs to have a column with the gene names called "gene" to proceed with this function.
+#' @param remove_ribosomal_genes default = T, remove ribosomal genes from FindMarkers results?
+#' @param create_ranked_vector default = T, create ranked vector to use with fgsea?
 #' @export
-#' @return if create_ranked_vector = T, a ranked vector with human genes as name and avg_logFC as values, to use for GSEA. if F, a dataframe.
-#'
-convert_FindMarkers_human <- function(fish_genelist, create_ranked_vector = T, remove_ribosomal_genes = T) {
-  # convert output of FindMarkers to human genes
-  # if create_ranked_vector = T: will return a ranked vector with human gene symbols and logFC, to be used for GSEA
+#' @return ranked vector or dataframe with converted human genes.
+
+convert_FindMarkers_human <- function(genelist, remove_ribosomal_genes = T, create_ranked_vector = T) {
+
+  require(data.table)
+  require(tidyverse)
 
   fish.human.convert.Z11 <- read.delim("/Users/hunterm/Documents/R/from_Nate/GRCz11_to_HS.txt")
-  fish.human.convert.Z11 <- fish.human.convert.Z11[fish.human.convert.Z11$DIOPT_Score > 6, ]
+  fish.human.convert.Z11 <- fish.human.convert.Z11[fish.human.convert.Z11$DIOPT_Score > 6,]
 
-  genelist <- fish_genelist
+  # check for presence of a "gene" column in df. If not, give an error message.
+  if (!"gene" %in% colnames(genelist)) {
+    stop("You must have a column with the gene names called \"gene\" in your FindMarkers results to proceed with this function.")
+  }
 
   if (remove_ribosomal_genes) {
-    genelist <- genelist %>% .[grep(pattern = "^rps|^rpl", x = .$gene, invert = T, ignore.case = T),]
+    genelist <- genelist[grep(pattern = "^rps|^rpl", x = genelist$gene, invert = T),]
   }
 
-  if (colnames(genelist)[1] == "p_val") {
-    genelist <- genelist %>% tibble::rownames_to_column(var = "fish_gene")
-  } else {
-    colnames(genelist)[1] <- "fish_gene"
-  }
+  # merge with conversion table
+  merged <- merge(x = genelist,
+                  y = fish.human.convert.Z11,
+                  by.x = "gene",
+                  by.y = "Zebrafish_Symbol")
 
-  merge <- merge(x = genelist,
-                 y = fish.human.convert.Z11,
-                 by.x = "fish_gene",
-                 by.y = "Zebrafish_Symbol")
+  # check for duplicates
+  dups <- merged$Human_Symbol[duplicated(merged$Human_Symbol)] %>% unique() %>% as.character()
 
-  h.genelist.c <- merge$Human_Symbol %>% as.character()
-  h.genelist <- list(h.genelist.c)
-
-  if (length(unique(duplicated(h.genelist.c))) == 1) {
-    message("No duplicated genes, conversion complete")
-
-    if (create_ranked_vector == T) {
-      h.genelist.vector <- merge %>% dplyr::select(Human_Symbol, avg_logFC) %>% deframe()
-      return(h.genelist.vector)
-    } else {
-      return(merge)
+  if (length(dups) >= 1) {
+    message("Some duplicated genes found, removing...")
+    # pull out duplicated genes
+    dups_data <- NULL
+    for (dup in dups) {
+      data <- merged[merged$Human_Symbol == dup,] %>% arrange(-avg_logFC)
+      dups_data[[dup]] <- data[1,] # take the expression data for the gene with the highest logFC
     }
+    dups_data <- rbindlist(dups_data)
 
-  } else {
-    message("Some genes are duplicated, removing...")
-
-    gene.dups <- as.character(unique(merge$Human_Symbol[duplicated(merge$Human_Symbol)]))
-    dups <- vector("list", length(gene.dups))
-
-    for (ii in 1:length(gene.dups)) {
-      genes <- merge[merge$Human_Symbol == gene.dups[ii], ] %>%
-        dplyr::arrange(desc(avg_logFC))
-      dups[[ii]] <- genes[1,]
-    }
-
-    dups <- data.table::rbindlist(dups)
-
-    nondups <- merge[grep(paste(pattern = gene.dups,
-                                collapse = "|"),
-                          x = merge$Human_Symbol,
-                          invert = TRUE), ]
-
-    test1 <- unique(duplicated(nondups$Human_Symbol))
-    test2 <- unique(duplicated(dups$fish_gene))
-
-    if (test1 == FALSE & test2 == FALSE) {
-      h.genelist <- rbind(dups, nondups)
-      test3 <- unique(duplicated(h.genelist$fish_gene))
-      test4 <- unique(duplicated(h.genelist$Human_Symbol))
-      if (test3 == FALSE & test4 == FALSE) {
-        message("Duplicated genes removed, conversion complete")
-
-        if (create_ranked_vector == T) {
-          h.genelist.vector <- h.genelist %>% dplyr::select(Human_Symbol, avg_logFC) %>% deframe()
-          return(h.genelist.vector)
-        } else {
-          return(h.genelist)
-        }
-
-      } else {
-        stop("Some duplicated genes weren't removed properly") }
-
+    # merge back with non duplicated genes
+    merged_new <- rbind(dups_data,
+                        merged[!merged$Human_Symbol %in% dups,])
+    message("Duplicated genes removed, conversion complete!")
+    if (create_ranked_vector) {
+      vector <- merged_new %>% dplyr::select(Human_Symbol, avg_logFC) %>% deframe()
+      return(vector)
     } else {
-      stop("Some duplicated genes weren't removed properly")
+      return(merged_new)
+    }
+  } else {
+    if (create_ranked_vector) {
+      message("No duplicated genes found, returning converted list.")
+      vector <- merged %>% dplyr::select(Human_Symbol, avg_logFC) %>% deframe()
+      return(vector)
+    } else {
+      return(merged)
     }
   }
 }
